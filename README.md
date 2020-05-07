40ed34a8b3e865eb5894fc46ce11299934f1f3dd
Henrik Hansen <henrikh2011@gmail.com>
2020-05-07 22:10:45 +0200

    Readme file update

--- README.md
+++ README.md
@@ -9,12 +9,24 @@ This repo is for a Arduino automated henhouse.
 MCU: Arduino Mini Pro for test
 (Upgrades to Arduino Nano 33 IoT)
 
+OnBoard LED PIN 13 for MCU programming connection
+MCU PIN: D13
+
 Motor Driver board: ULN2003 stepper motor driver and 28BYJ-48 - 5VDC Steppermotor. MOTOR TEST for henhouse door open/Close
+MCU PIN: D8, D9, D10, D11 - OUTPUT
 
 Photoresistor, light sensor
+MCU PIN: A0 - INPUT
 
-230VAC 10A relay, power On/Off for 230VAC light
+MagnetSensorSwitch for motor stop control
+MCU PIN: D2, D3 - INPUT
+
+TouchSwitch for critical stop
+MCU PIN: D4 - INPUT
 
 Push button for manuel motorcontroll and manuel light on/off
+MCU PIN: D5, D6 - INPUT
 
+230VAC 10A relay, power On/Off for 230VAC light
+MCU PIN: D12 - OUTPUT
 
--- src/main.cpp
+++ src/main.cpp
@@ -30,11 +30,26 @@ void loop() {
 #define MotorInterfaceType 8
 // Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper library with 28BYJ-48 stepper motor:
 AccelStepper stepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, motorPin2, motorPin4);
+
+const int lightSensorPin01 = A0
+const int pushButtomMotorUp = 
+
 void setup() {
+  // initialize digital pin LED_BUILTIN as an output. For MCU connection test
+  pinMode(LED_BUILTIN, OUTPUT);
+}  
   // Set the maximum steps per second:
   stepper.setMaxSpeed(1000);
 }
 void loop() {
+  //MCU connection test
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  
+ 
   // Set the current position to 0:
   stepper.setCurrentPosition(0);
   // Run the motor forward at 500 steps/second until the motor reaches 4096 steps (1 revolution):
