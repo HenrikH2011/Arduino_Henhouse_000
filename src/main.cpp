61bcd80662f998ff1dd18db10049846c8cf7a803
Henrik Hansen <henrikh2011@gmail.com>
2020-05-07 20:45:38 +0200

    Steppermotor Driver AccelStepper library code update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,62 @@
-#include <Arduino.h>
 
+
+/*
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(2000);                       // wait for a second
+}
+*/
+
+/* Example sketch to control a 28BYJ-48 stepper motor with ULN2003 driver board, AccelStepper and Arduino UNO: number of steps/revolutions. More info: https://www.makerguides.com */
+// Include the AccelStepper library:
+#include <Arduino.h>
+#include <AccelStepper.h>
+// Motor pin definitions:
+#define motorPin1  8      // IN1 on the ULN2003 driver
+#define motorPin2  9      // IN2 on the ULN2003 driver
+#define motorPin3  10     // IN3 on the ULN2003 driver
+#define motorPin4  11     // IN4 on the ULN2003 driver
+// Define the AccelStepper interface type; 4 wire motor in half step mode:
+#define MotorInterfaceType 8
+// Initialize with pin sequence IN1-IN3-IN2-IN4 for using the AccelStepper library with 28BYJ-48 stepper motor:
+AccelStepper stepper = AccelStepper(MotorInterfaceType, motorPin1, motorPin3, motorPin2, motorPin4);
+void setup() {
+  // Set the maximum steps per second:
+  stepper.setMaxSpeed(1000);
+}
+void loop() {
+  // Set the current position to 0:
+  stepper.setCurrentPosition(0);
+  // Run the motor forward at 500 steps/second until the motor reaches 4096 steps (1 revolution):
+  while (stepper.currentPosition() != 4096) {
+    stepper.setSpeed(500);
+    stepper.runSpeed();
+  }
+  delay(1000);
+  // Reset the position to 0:
+  stepper.setCurrentPosition(0);
+  // Run the motor backwards at 1000 steps/second until the motor reaches -4096 steps (1 revolution):
+  while (stepper.currentPosition() != -4096) {
+    stepper.setSpeed(-1000);
+    stepper.runSpeed();
+  }
+  delay(1000);
+  // Reset the position to 0:
+  stepper.setCurrentPosition(0);
+  // Run the motor forward at 1000 steps/second until the motor reaches 8192 steps (2 revolutions):
+  while (stepper.currentPosition() != 8192) {
+    stepper.setSpeed(1000);
+    stepper.runSpeed();
+  }
+  delay(3000);
 }
